d02c6efffc22285362aeff874d0202d3eddb135b
Ilya Demukh <demukh.ilya@gmail.com>
2018-03-17 12:33:02 +0300

    use size prop instead of explicit height and width

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,11 +4,16 @@ import testAvatar from './test-avatar.jpg';
 import './Avatar.css';
 
 function Avatar(props) {
-  let {height = '300px', width = '300px'} = props;
+  let size = props.size;
+  if (['s', 'm', 'l'].indexOf(size) === -1) {
+    size = 'm';
+  }
+  let modifier = `avatar_size_${size}`;
+
   let src = props.src || testAvatar;
 
   return (
-    <img src={src} className="avatar" style={{height, width}} alt="avatar" />
+    <img src={src} className={`avatar ${modifier}`} alt="avatar" />
   )
 }
 
