b14817025f5bdd1fcd5a4fbc28dcbe4abb61c2e7
pistch <pistch@yandex.ru>
2018-03-22 03:44:22 +0300

    Исправлен хидер, package.json и ребилд сторибука

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Header.module.css';
 
-class Header extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      normalizedText: '',
-    };
-  }
-
-  componentWillMount() {
-    this.normalizeText();
-  }
-
-  componentWillUpdate() {
-    this.normalizeText();
-  }
-
-  normalizeText() {
-    const text = typeof this.props.children === 'string' ? this.props.children : '';
-
-    this.setState({
-      normalizedText: text,
-    });
-  }
-
-  render() {
-    return (
-      <div className={styles.header}>
-        <div className={styles.left_slot}>{this.props.left || null}</div>
-        <div className={styles.text}>
-          {this.state.normalizedText}
-        </div>
-        <div className={styles.right_slot}>{this.props.right || null}</div>
+function Header({ children, left, right }) {
+  return (
+    <div className={styles.header}>
+      <div className={styles.left_slot}>{left}</div>
+      <div className={styles.text}>
+        {children}
       </div>
-    );
-  }
+      <div className={styles.right_slot}>{right}</div>
+    </div>
+  );
 }
 
-
 Header.propTypes = {
-  left: PropTypes.string.isRequired,
-  right: PropTypes.string.isRequired,
-  children: PropTypes.element.isRequired,
+  left: PropTypes.node,
+  right: PropTypes.node,
+  children: PropTypes.string,
+};
+
+Header.defaultProps = {
+  left: '',
+  right: '',
+  children: '',
 };
 
 export default Header;
