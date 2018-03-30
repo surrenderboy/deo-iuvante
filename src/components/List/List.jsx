983d78df04df1a28a4bba2afc80c8ea7f9b2d6a5
pistch <glushenkoa@156-142-57-86.cisco-wifi.beltelecom.by>
2018-03-30 21:24:46 +0300

    Переработка компонента списка
    Компонент списка теперь исключительно вспомогательный

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './List.css';
+import styles from './List.module.css';
 
-const List = props => (
-  <div {...props} className="list">{props.children}</div>
-);
+class List extends Component {
+  renderListItems() {
+    const { ListItem } = this.props;
+
+    return (this.props.listItemProps.map(item => (
+      <ListItem {...item} key={Math.random().toString(32).slice(2)} />
+    )));
+  }
+
+  render() {
+    return (
+      <div className={this.props.className}>
+        {this.renderListItems()}
+      </div>
+    );
+  }
+}
 
 List.propTypes = {
-  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  ListItem: PropTypes.func.isRequired,
+  listItemProps: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+List.defaultProps = {
+  className: styles.list,
 };
 
 export default List;
