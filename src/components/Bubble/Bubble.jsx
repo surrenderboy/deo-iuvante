296cefc595b5b612cc56d2f12854ccb3d5134684
Ivan Kononov <ikononov94@gmail.com>
2018-04-11 16:25:08 +0300

    Добавлен prop created_at в Bubble, перенос строки с длинным словом

--- src/components/Bubble/Bubble.jsx
+++ src/components/Bubble/Bubble.jsx
@@ -7,7 +7,7 @@ const Bubble = props =>
     <div className="Bubble" key={Date.now()} >
       <div className={props.isOwner ? 'Bubble_owner_yes' : ''}>
         <span className="Bubble__message">
-          {props.message}
+          <span className="Bubble__message-text">{props.message}</span>
           <span className="Bubble_created-at">
             {new Date(props.created_at).toLocaleString(
         'ru',
@@ -27,6 +27,7 @@ Bubble.propTypes = {
   isOwner: PropTypes.bool.isRequired,
   message: PropTypes.string.isRequired,
   isRead: PropTypes.bool.isRequired,
+  created_at: PropTypes.number.isRequired,
 };
 
 export default Bubble;
