426521a519e5797bed09d99df988e68d75ba3dc1
Alexander Glushenko <pistch@yandex.ru>
2018-04-14 13:22:26 +0300

    Update rooms.js

--- src/actions/rooms.js
+++ src/actions/rooms.js
@@ -2,6 +2,7 @@ import api from '../api';
 
 import { FETCH_ROOMS_SUCCESS } from '../actions/types';
 
+// eslint-disable-next-line import/prefer-default-export
 export const fetchRooms = () => (dispatch) => {
   api.getRooms()
     .then(rooms => dispatch({ type: FETCH_ROOMS_SUCCESS, payload: rooms }));
