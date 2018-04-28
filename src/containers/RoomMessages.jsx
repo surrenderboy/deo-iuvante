4264a32bcb380647375568fb40fcd285d15eafbd
Ilya Demukh <demukh.ilya@gmail.com>
2018-04-28 16:50:19 +0300

    read messages on chat update

--- src/containers/RoomMessages.jsx
+++ src/containers/RoomMessages.jsx
@@ -5,11 +5,27 @@ import { fetchMessages, sendMessage } from '../actions/messages';
 import { fetchUser, fetchUsers } from '../actions/users';
 
 import Chat from '../components/Chat/Chat';
+import { markAllUnreadMessages } from '../actions/rooms';
 
 class RoomMessages extends Component {
   componentDidMount() {
     this.props.fetchMessages(this.props.roomId);
     this.props.fetchUsers();
+  }
+
+  componentDidUpdate() {
+    this.clearTimeout();
+
+    this.timeout = setTimeout(() => this.props.readMessages(this.props.roomId), 500);
+  }
+
+  componentWillUnmount() {
+    this.clearTimeout();
+  }
+
+  clearTimeout() {
+    // eslint-disable-next-line no-unused-expressions
+    this.timeout && clearTimeout(this.timeout);
   }
 
   render() {
@@ -44,6 +60,7 @@ const mapDispatchToProps = dispatch => ({
   sendMessage: (roomId, message) => dispatch(sendMessage(roomId, message)),
   fetchMessages: roomId => dispatch(fetchMessages(roomId)),
   fetchUser: _id => dispatch(fetchUser(_id)),
+  readMessages: roomId => dispatch(markAllUnreadMessages(roomId)),
   fetchUsers: filter => dispatch(fetchUsers(filter)),
   dispatch,
 });
@@ -60,6 +77,7 @@ RoomMessages.propTypes = {
   sendMessage: PropTypes.func.isRequired,
   roomId: PropTypes.string.isRequired,
   currentUserId: PropTypes.string.isRequired,
+  readMessages: PropTypes.func.isRequired,
   isFetchingMessages: PropTypes.bool,
   messages: PropTypes.arrayOf(PropTypes.object),
   users: PropTypes.arrayOf(PropTypes.object).isRequired,
