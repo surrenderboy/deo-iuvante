be44b1f54f2b9e69438de25bf0b80d086d04c477
Ilya Demukh <demukh.ilya@gmail.com>
2018-05-10 19:49:22 +0300

    AppLayout can render footer

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -5,32 +5,34 @@ import Header from '../Header/Header';
 
 import styles from './AppLayout.module.css';
 
-function AppLayout({
+const AppLayout = ({
   headerText,
   children,
   headerLeft,
   headerRight,
-}) {
-  return (
-    <div className={styles.app_layout}>
-      <Header left={headerLeft} right={headerRight} className={styles.header}>{headerText}</Header>
-      <div className={styles.main_area}>
-        {children}
-      </div>
+  footer,
+}) => (
+  <div className={styles.app_layout}>
+    <Header left={headerLeft} right={headerRight} className={styles.header}>{headerText}</Header>
+    <div className={styles.main_area}>
+      {children}
     </div>
-  );
-}
-
+    <div className={styles.footer}>
+      {footer}
+    </div>
+  </div>
+);
 AppLayout.propTypes = {
   headerText: PropTypes.node.isRequired,
   children: PropTypes.element.isRequired,
   headerLeft: PropTypes.node,
   headerRight: PropTypes.node,
+  footer: PropTypes.node,
 };
-
 AppLayout.defaultProps = {
-  headerLeft: '',
-  headerRight: '',
+  headerLeft: null,
+  headerRight: null,
+  footer: null,
 };
 
 export default AppLayout;
