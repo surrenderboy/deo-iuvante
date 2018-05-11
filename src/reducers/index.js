df3e08679ec1ade0a49c5e38cd522aa81b4bdc20
Ilya Demukh <demukh.ilya@gmail.com>
2018-05-11 10:01:39 +0300

    added isFetching reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import currentUser from './currentUser';
 import rooms from './rooms';
 import messages from './messages';
 import users from './users';
+import isFetching from './isFetching';
 
 export default combineReducers({
   currentUser,
   messages,
   rooms,
   users,
+  isFetching,
 });
---
+++ src/reducers/isFetching.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+
+import * as ActionTypes from '../actions/types';
+
+const room = (state = false, action) => {
+  switch (action.type) {
+    case ActionTypes.FETCH_ROOM_START: return true;
+    case ActionTypes.FETCH_ROOM_END: return false;
+    default: return state;
+  }
+};
+
+export default combineReducers({
+  room,
+});
